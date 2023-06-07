5338ababb13fb02d6f7a488bb2869a3f07cc6338
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-06 18:21:49 -0700

    added service for recipes and tested on homepage html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../services/recipe.service';
+import { RecipeModel } from '../models/recipe.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  recipes!: RecipeModel[];
+
+  constructor(private recipeService: RecipeService) { }
+
+  grabRecipes(): void {
+    this.recipeService.getRecipes().subscribe(
+      recipes => {
+        // assign the recipe to the recipe property
+        this.recipes = recipes;
+      });
+  }
+
+  ngOnInit(): void {
+    this.grabRecipes();
+  }
 
 }
