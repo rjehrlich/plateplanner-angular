e9ad7184102e1013c3f068faa5a03fefc91eb8bb
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-06 21:49:06 -0700

    created grocerylist service to handle generate grocery list

--- src/app/services/grocery-list.service.ts
+++ src/app/services/grocery-list.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GroceryListModel } from '../models/grocery-list.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceryListService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  generateGroceryList(recipeIds: number[]): Observable<GroceryListModel> {
+    return this.http.post<GroceryListModel>('/grocery-list', recipeIds);
+  }
 }
