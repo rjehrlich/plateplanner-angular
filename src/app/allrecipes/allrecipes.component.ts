ca73432471383bc213e9d9f25fd5f019e94cd82d
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-07 13:40:20 -0700

    tested ingredient names display, then update create groceries method to pass the list

--- src/app/allrecipes/allrecipes.component.ts
+++ src/app/allrecipes/allrecipes.component.ts
@@ -14,15 +14,25 @@ export class AllrecipesComponent implements OnInit {
 
   constructor(private recipeService: RecipeService, private router: Router) { }
 
-  // method to create the grocery list from recipes selected by the user
+  // method to create an ingredient list from recipes selected by the user
   createGroceries(): void {
-    // get recipeIds of selected recipes
+    // get all selected recipes
     const selectedRecipes = this.recipes
-    .filter(recipe => recipe.selected)
-    .map(recipe => recipe.id);
+    .filter(recipe => recipe.selected);
 
-    // then send the user to grocerylist page passing the recipeIds
-    this.router.navigate(['/grocery-list'], { queryParams: { recipeIds: selectedRecipes.join(',') } });
+    // aggregate the ingredients from the selected recipes
+    const ingredients: string[] = [];
+      // loop through the selected recipes and the ingredients
+    selectedRecipes.forEach(recipe => {
+      // for each extract the name
+      recipe.ingredients.forEach(ingredient => {
+        // push the ingredient name to the ingredients array
+        ingredients.push(ingredient.name);
+      });
+    });
+
+    // then send the user to grocerylist page passing the ingredients information
+    this.router.navigate(['/grocery-list'], { queryParams: { ingredients: ingredients.join(',') } });
   }
 
   ngOnInit(): void {
