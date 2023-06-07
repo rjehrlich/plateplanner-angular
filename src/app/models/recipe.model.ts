ebb5e59075f74d3731723aa67b72d1b2bee0269e
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-06 17:04:15 -0700

    added recipe interface

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,2 +1,12 @@
+import { IngredientModel } from "./ingredient.model";
+
 export interface RecipeModel {
+    id: number;
+    title: string;
+    description: string;
+    prepTime: string;
+    cookTime: string;
+    instructions: string;
+    sourceUrl: string;
+    ingredients: IngredientModel[];
 }
