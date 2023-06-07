a63ca80979c498a9d1f07f11a6cb3f8f2085ef74
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-07 13:54:17 -0700

    set up grocerylist component to handle query params of ingredients

--- src/app/grocerylist/grocerylist.component.ts
+++ src/app/grocerylist/grocerylist.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-grocerylist',
   templateUrl: './grocerylist.component.html',
   styleUrls: ['./grocerylist.component.css']
 })
-export class GrocerylistComponent {
+export class GrocerylistComponent  implements OnInit {
+  // declare ingredients array to handle query parameter of ingredient names
+  ingredients: string[] = [];
+
+  // allow access to current route queries
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // subscribe to query observable that activateroute gives
+    this.route.queryParamMap.subscribe(params => {
+      // get that value of ingredients query parameter and assign to a variable
+      const ingredientNames = params.get('ingredients');
+        // if there are ingredients, then split them and assign to ingredient variable array
+        if (ingredientNames) {
+          this.ingredients = ingredientNames.split(',');
+        }
+    });
+  }
 
 }
