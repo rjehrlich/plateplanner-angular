6cfd093c3fef6f096ab66b2e75d694d32f5418f3
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-07 12:45:03 -0700

    updated allrecipes html to loop through recipes

--- src/app/allrecipes/allrecipes.component.ts
+++ src/app/allrecipes/allrecipes.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
+import { RecipeModel } from '../models/recipe.model';
 
 @Component({
   selector: 'app-allrecipes',
   templateUrl: './allrecipes.component.html',
   styleUrls: ['./allrecipes.component.css']
 })
-export class AllrecipesComponent {
+export class AllrecipesComponent implements OnInit {
+
+  recipes!: RecipeModel[];
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe(recipes => {
+      this.recipes = recipes;
+    });
+  }
 
 }
