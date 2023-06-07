5dc795db8c4d53a831fc0525d0d9f2e716a9b3bc
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-07 08:56:05 -0700

    setup grocerylist route and connect in nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AllrecipesComponent } from './allrecipes/allrecipes.component';
+import { GrocerylistComponent } from './grocerylist/grocerylist.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'recipes',
     component: AllrecipesComponent,
+  },
+  {
+    path: 'grocery-list',
+    component: GrocerylistComponent
   }
 ];
 
