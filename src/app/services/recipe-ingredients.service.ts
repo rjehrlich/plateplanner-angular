b4ff84a3502ca1aa7832c633d7a654778ce3a017
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-07 22:27:15 -0700

    added recipeingredients service

---
+++ src/app/services/recipe-ingredients.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RecipeIngredientModel } from '../models/recipe-ingredient-model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeIngredientsService {
+  private apiUrl = 'http://localhost/8080';
+
+  constructor(private http: HttpClient) { }
+
+  // get recipe ingredients by recipe ID
+  getRecipeIngredientsByRecipeId(recipeId: number): Observable<RecipeIngredientModel[]> {
+    const url = `${this.apiUrl}/recipes/${recipeId}/ingredients`;
+    return this.http.get<RecipeIngredientModel[]>(url);
+  }
+
+  // get recipe ingredients by ingredient ID
+  getRecipeIngredientsByIngredientId(ingredientId: number): Observable<RecipeIngredientModel[]> {
+    const url = `${this.apiUrl}/ingredients/${ingredientId}/recipes`;
+    return this.http.get<RecipeIngredientModel[]>(url);
+  }
+}
