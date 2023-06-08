7f2cc7c87e4e5fde5a273a7000214a0b1b4e03e2
rjsehrlich <racheljsehrlich@gmail.com>
2023-06-07 18:23:13 -0700

    updated allrecipes router query to handle addition of quantity

--- src/app/allrecipes/allrecipes.component.ts
+++ src/app/allrecipes/allrecipes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../services/recipe.service';
 import { RecipeModel } from '../models/recipe.model';
 import { Router } from '@angular/router';
+import { RecipeIngredientModel } from '../models/recipe-ingredient-model';
 
 @Component({
   selector: 'app-allrecipes',
@@ -22,20 +23,26 @@ export class AllrecipesComponent implements OnInit {
     //console.log(selectedRecipes);
 
     // aggregate the ingredients from the selected recipes
-    const ingredients: string[] = [];
+    const ingredients: RecipeIngredientModel[] = [];
+    //const ingredients: string[] = [];
       // loop through the selected recipes and the ingredients
-    selectedRecipes.forEach(recipe => {
-      // for each extract the name
-      recipe.ingredients.forEach(ingredient => {
-        // push the ingredient name to the ingredients array
-        ingredients.push(ingredient.name);
+    selectedRecipes.forEach((recipe) => {
+      // for each recipeIngredient in recipeIngredients
+      recipe.recipeIngredients.forEach((recipeIngredient) => {
+        const ingredient: RecipeIngredientModel = {
+          recipe: recipeIngredient.recipe,
+          ingredient: recipeIngredient.ingredient,
+          quantity: recipeIngredient.quantity,
+        };
+        // push the ingredients and quantity to the ingredients array
+        ingredients.push(ingredient);
       });
     });
     // console.log(selectedRecipes);
     // console.log(ingredients);
     
     // then send the user to grocerylist page passing the ingredients information
-    this.router.navigate(['grocery-list'], { queryParams: { ingredients: ingredients.join(',') } });
+    this.router.navigate(['grocery-list'], { queryParams: { ingredients: JSON.stringify(ingredients), } });
   }
 
   ngOnInit(): void {
